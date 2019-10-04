a6781f3fbd89c847c32947040cbbfd24314162df
Memeken <coralie2319@hotmail.fr>
2019-10-04 20:57:38 +0200

    first commit

---
+++ indexaleksander.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<p>
+		<?php
+		$variable = "platon \"atlantide\" est belle";
+		$phraseTest = "l'éléphant devant l'enveloppe illuminé d'une carte postale joyeuse";
+
+		echo "$variable" . '<br>';
+
+		echo strlen($variable) . "<br>";
+
+		echo 42 . "<br>";
+
+		echo "42" . "<br>";
+
+		echo "l'éléphant devant l'enveloppe illuminé d'une carte postale joyeuse";
+
+		echo "$phraseTest";
+
+	?>
+	</p>
+
+	<?php
+		$variable = 'platon atlantide est belle';
+
+		echo "$variable" . '<br>';
+
+		echo strlen($variable) . "<br>";
+
+		echo 42 . "<br>";
+
+		echo "42" . "<br>";
+
+		echo 'l\'éléphant devant l\'enveloppe illuminé d\'une carte postale joyeuse' . "<br>";
+
+	?>
+
+	<?php
+
+	 $var1= true;
+	 echo gettype($var1) . "\n";
+
+	 ?>
+
+	 <?php // exo 9
+	 	$mouchoir ='sale';
+	 	$sale = 'mouchoir';
+	 	$temporaire = &$mouchoir;
+	 	$mouchoir = &$sale;
+	 	$sale = &$temporaire;
+
+	 	echo "<br>";
+
+	 	echo $mouchoir;
+	 	echo $sale;
+	 ?>
+
+	 <? php // exo 10
+
+	 ?>
+
+	
+</body>
+</html>
